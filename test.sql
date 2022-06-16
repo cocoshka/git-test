6327793cf1026096a7e931c2250530d8dcde8711
Marcin Kokoszka <mcocoshka@gmail.com>
2022-06-16 13:46:10 +0200

    Add address table

--- test.sql
+++ test.sql
@@ -1,1 +1,9 @@
-
+CREATE TABLE `address` (
+  `address_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `city` VARCHAR(255) NOT NULL,
+  `zipcode` VARCHAR(16) NOT NULL,
+  `street` VARCHAR(255) NOT NULL,
+  `street_number` VARCHAR(16) NOT NULL,
+  `flat_number` VARCHAR(16) NOT NULL
+)
